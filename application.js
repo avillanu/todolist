1378773b7746beda0fd535e1a5c8796b76d4074d
Alex <alexv@new-host.home>
2014-01-30 06:29:32 -0500

    -initial

---
+++ application.js
@@ -0,0 +1,35 @@
+var counter = 1;
+
+function deleteItem (item) {
+alert(item);
+}
+
+function addResults (form) {
+results = form.inputbox.value;
+var title = 'div' + counter;
+counter++;
+var div = document.createElement("div");
+div.id = title;
+div.style.width = "300px";
+div.style.height = "30px";
+div.style.color = "black";
+div.innerHTML = results;
+document.getElementById('unchecked').appendChild(div);
+
+var button = document.createElement("input");
+	var buttonid = 'button' + counter;
+	button.id = title;
+    button.type = "button";
+    button.counter = counter;
+    button.value = "check";
+    button.onclick = function(){
+    div.style.setProperty("text-decoration","line-through");
+    document.getElementById('checked').appendChild(div);
+   var elem = document.getElementById(title);
+    elem.parentNode.removeChild(elem);
+    $(this).remove();
+    };
+
+
+document.getElementById('unchecked').appendChild(button);
+};
